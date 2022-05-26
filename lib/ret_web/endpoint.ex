7c828422bc5d646442cde909a2272ac6ca703386
lonycell <lonycell@gmail.com>
2022-05-26 22:27:49 +0900

    wip

--- lib/ret_web/endpoint.ex
+++ lib/ret_web/endpoint.ex
@@ -1,4 +1,5 @@
 defmodule RetWeb.Endpoint do
+  require Logger
   use Phoenix.Endpoint, otp_app: :ret
   use Sentry.Phoenix.Endpoint
   use Absinthe.Phoenix.Endpoint
@@ -10,8 +11,10 @@ defmodule RetWeb.Endpoint do
 
   def allowed_origin?(%URI{host: host, port: port, scheme: scheme}) do
     if get_cors_origins() === ["*"] do
+      Logger.info("allowed_origin --> *")
       true
     else
+      Logger.info("allowed_origin --> ?")
       get_cors_origin_urls() |> Enum.any?(&(&1.host == host && &1.port == port && &1.scheme == scheme))
     end
   end
