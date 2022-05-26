deea299ef93d02adf8fb77ee638095571b36f4b9
김상수 <lonycell@gmail.com>
2022-05-26 20:33:18 +0900

    [CUSTOME] retinum - 포트 조사

--- lib/ret/peerage_provider.ex
+++ lib/ret/peerage_provider.ex
@@ -4,7 +4,7 @@ defmodule Ret.PeerageProvider do
   def poll do
     #FIXME
     #Habitat.get_service_members("reticulum")
-    Habitat.get_service_members()
+    Habitat.get_service_members(nil)
     |> Enum.map(fn {host, _ip} -> host |> hostname_to_erlang_node end)
     |> Enum.filter(&(&1 != Node.self()))
   end
