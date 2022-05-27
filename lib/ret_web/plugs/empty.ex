9ffe83b43a464862f0ceeebf1ff8e618c772de0f
김상수 <lonycell@gmail.com>
2022-05-27 12:38:49 +0900

    [CUSTOME] retinum - Test - Empty response

--- lib/ret_web/plugs/empty.ex
+++ lib/ret_web/plugs/empty.ex
@@ -1,9 +1,11 @@
 defmodule RetWeb.Plugs.Empty do
+  require Logger
   alias Plug.Conn
 
   def init([]), do: []
 
   def call(conn, _opts) do
+      Logger.debug("FIXME: Empty response ...")
       conn
       |> Plug.Conn.send_resp(200, [])
       |> Plug.Conn.halt()
