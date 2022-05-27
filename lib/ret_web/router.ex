089dac2de6ff0705cf43a5769122736ad10f2466
김상수 <lonycell@gmail.com>
2022-05-27 13:17:48 +0900

    [CUSTOME] retinum - Test - Empty response

--- lib/ret_web/router.ex
+++ lib/ret_web/router.ex
@@ -249,7 +249,7 @@ defmodule RetWeb.Router do
     pipe_through(
       # [:secure_headers, :parsed_body, :browser] ++
       #   if(Mix.env() == :prod, do: [:ssl_only, :canonicalize_domain], else: [])
-      [:secure_headers, :parsed_body, :browser, :browser_for_benchmark]
+      [:secure_headers, :parsed_body, :browser]
     )
 
     #FIXME get("/*path", PageController, only: [:index])
