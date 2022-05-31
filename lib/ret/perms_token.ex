237498ac1f2037739d46a76d487526d39982108f
김상수 <lonycell@gmail.com>
2022-05-31 10:07:36 +0900

    [RETINUM] Note for FIXME !

--- lib/ret/perms_token.ex
+++ lib/ret/perms_token.ex
@@ -20,7 +20,7 @@ defmodule Ret.PermsToken do
   def resource_from_claims(_), do: nil
 
   def token_for_perms(perms) do
-    Logger.debug("FIXME: Ret.PermsToken - token_for_perms - perms%{"perms"}")
+    Logger.debug("FIXME: Ret.PermsToken - token_for_perms - perms%{perms}")
 
     {:ok, token, _claims} =
       Ret.PermsToken.encode_and_sign(
@@ -37,12 +37,12 @@ end
 
 defmodule Ret.PermsTokenSecretFetcher do
   def fetch_signing_secret(mod, _opts) do
-    Logger.debug("FIXME: Ret.PermsToken - fetch_signing_secret - mod##1 %{"mod"}")
+    Logger.debug("FIXME: Ret.PermsToken - fetch_signing_secret - mod##1 %{mod}")
     {:ok, Application.get_env(:ret, mod)[:perms_key] |> JOSE.JWK.from_pem()}
   end
 
   def fetch_verifying_secret(mod, _token_headers, _opts) do
-    Logger.debug("FIXME: Ret.PermsToken - fetch_verifying_secret - mod#2 %{"mod"}")
+    Logger.debug("FIXME: Ret.PermsToken - fetch_verifying_secret - mod#2 %{mod}")
     {:ok, Application.get_env(:ret, mod)[:perms_key] |> JOSE.JWK.from_pem()}
   end
 end
