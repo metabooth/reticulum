3fbe62f8a266b2d4fbe4733d7867e15e5efa17ac
김상수 <lonycell@gmail.com>
2022-05-31 10:09:34 +0900

    [RETINUM] Note for FIXME !

--- lib/ret/perms_token.ex
+++ lib/ret/perms_token.ex
@@ -3,6 +3,8 @@ defmodule Ret.PermsToken do
   PermsTokens grant granular permissions to users in various contexts.
   They are signed with an RSA algorithm so that external systems can verify tokens with our corresponding public key.
   """
+  require Logger
+
   use Guardian, otp_app: :ret, secret_fetcher: Ret.PermsTokenSecretFetcher, allowed_algos: ["RS512"]
 
   def subject_for_token(_resource, %{"account_id" => account_id, "hub_id" => hub_id}) do
