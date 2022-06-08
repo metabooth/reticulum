ba19e9f5ea745c803b944cf1c5dc3bb45706f231
김상수 <lonycell@gmail.com>
2022-06-08 14:17:22 +0900

    [RETINUM] Add nginx.conf sample.

--- config/config.exs
+++ config/config.exs
@@ -19,11 +19,6 @@ config :mime, :types, %{
   "application/vnd.api+json" => ["json-api"],
   "model/gltf+json" => ["gltf"],
   "model/gltf-binary" => ["glb"],
-
-  # FIXME: TEST
-  "model/fbx" => ["fbx"],
-  "application/vnd.ms-excell" => ["excell"],
-
   "application/vnd.spoke.scene" => ["spoke"],
   "application/vnd.pgrst.object+json" => ["json"],
   "application/json" => ["json"],
